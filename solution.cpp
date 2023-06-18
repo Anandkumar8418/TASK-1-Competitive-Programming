7e7d0a3fbfca0b120e00223d374021625d82e598
WhiteDevil4648 <119500199+Anandkumar8418@users.noreply.github.com>
2023-06-19 05:03:27 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -8,7 +8,6 @@ using namespace std;
 #define pb push_back
 #define rep(i, a, b) for (int i = a; i < b; i++)
 
-
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -16,42 +15,51 @@ int main()
 
     int n;
     cin >> n;
-    vector<pii> vec;
+    vector<pair<pii, int>> inputvec;
+    vector<pair<pii, int>> SortingbyRight;
     for (int i = 0; i < n; i++)
     {
         int a, b;
         cin >> a >> b;
         pii p = {a, b};
-        vec.pb(p);
+        pair<pii, int> p2 = {p, i + 1};
+        inputvec.pb(p2);
+        pii temppair = {b, a};
+        pair<pii, int> temppair2 = {temppair, i + 1};
+        SortingbyRight.pb(temppair2);
+    }
+    vector<pair<pii, int>> SortingbyLeft = inputvec;
+    sort(all(SortingbyLeft));
+    v32 leftleg(n);
+    for (int i = 0; i < n; i++)
+    {
+        leftleg[i] = SortingbyLeft[i].second;
+    }
+    sort(all(SortingbyRight));
+    v32 rightleg(n);
+    for (int i = 0; i < n; i++)
+    {
+        rightleg[i] = SortingbyRight[i].second;
     }
     int count1 = 0;
     for (int i = 0; i < n; i++)
     {
-        if (vec[i].first != i + 1)
+        if (leftleg[i] != i + 1)
         {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (vec[j].first == i + 1)
-                {
-                    count1++;
-                    vec[j].first = vec[i].first;
-                    vec[i].first = i + 1;
-                }
-            }
+            auto it = find(all(leftleg), i + 1);
+            int index = it - leftleg.begin();
+            leftleg[index] = leftleg[i];
+            leftleg[i] = i + 1;
+            count1++;
         }
-        if (vec[i].second != i + 1)
+        if (rightleg[i] != i + 1)
         {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (vec[j].second == i + 1)
-                {
-                    count1++;
-                    vec[j].second = vec[i].second;
-                    vec[i].second = i + 1;
-                }
-            }
+            auto it2 = find(all(rightleg), i + 1);
+            int index2 = it2 - rightleg.begin();
+            rightleg[index2] = rightleg[i];
+            rightleg[i] = i + 1;
+            count1++;
         }
     }
-
     cout << count1;
 }
